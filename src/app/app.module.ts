edffced8a6517d9fbe4a9fc3e69702f599440bfc
raquelsr <raquelserranor@hotmail.com>
2017-12-21 13:21:51 +0100

    Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,79 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HttpService } from './core/http.service';
 import { ApiItemsService } from './api-items/api-items.service';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { ApiItemsComponent } from './api-items/api-items.componet';
+import { MaterialComponent } from './material/material.component';
+import {
+  MatAutocompleteModule, MatButtonModule, MatButtonToggleModule,
+  MatCardModule, MatCheckboxModule, MatChipsModule,
+  MatDatepickerModule, MatDialogModule, MatExpansionModule,
+  MatGridListModule, MatIconModule, MatInputModule,
+  MatListModule, MatMenuModule, MatNativeDateModule,
+  MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+  MatRadioModule, MatRippleModule, MatSelectModule,
+  MatSidenavModule, MatSliderModule, MatSlideToggleModule,
+  MatSnackBarModule, MatSortModule, MatTableModule,
+  MatTabsModule, MatToolbarModule, MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+import { CdkTableModule } from '@angular/cdk/table';
 
 
 @NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
+export class DemoMaterialModule { }
+
+@NgModule({
   declarations: [
-    AppComponent, ApiItemsComponent
+    AppComponent, ApiItemsComponent, MaterialComponent
   ],
   imports: [
-    BrowserModule, HttpModule, FormsModule
+    BrowserModule, HttpModule, FormsModule, DemoMaterialModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule
+
   ],
   providers: [HttpService, ApiItemsService],
   bootstrap: [AppComponent]
