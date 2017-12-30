2e7a7dccec4f15b6c27273534c65190730fd2eeb
raquelsr <raquelserranor@hotmail.com>
2017-12-30 21:30:42 +0100

    Limpieza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 
+
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HttpService } from './core/http.service';
 import { ApiItemsService } from './api-items/api-items.service';
-import { HttpModule, JsonpModule } from '@angular/http';
-import { ApiItemsComponent } from './api-items/api-items.component';
-import { MaterialComponent } from './material/material.component';
+
+import { CrearItemCallDialog, CrearItemDialog } from './crearItem/crearItem_dialog.component';
+import { MostrarItems}  from './mostrarItems/mostrarItems.component';
+import { EliminarItemDialog, EliminarItemCallDialog } from './eliminarItem/eliminarItem_dialog.component';
+import { EditarItemDialog, EditarItemCallDialog } from './editarItem/editarItem_dialog.component';
+import { VerItemDialog, VerItemCallDialog } from './verItem/verItem_dialog.component';
+
 import {
   MatAutocompleteModule, MatButtonModule, MatButtonToggleModule,
   MatCardModule, MatCheckboxModule, MatChipsModule,
@@ -25,11 +31,6 @@ import {
 } from '@angular/material';
 import { CdkTableModule } from '@angular/cdk/table';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { CrearItemCallDialog, CrearItemDialog} from './crearItem/crearItem_dialog.component';
-import { MostrarItems} from './mostrarItems/mostrarItems.component';
-import { EliminarItemDialog, EliminarItemCallDialog} from './eliminarItem/eliminarItem_dialog.component';
-import { EditarItemDialog, EditarItemCallDialog} from './editarItem/editarItem_dialog.component';
-import { VerItemDialog, VerItemCallDialog} from './verItem/verItem_dialog.component';
 
 @NgModule({
   exports: [
@@ -71,7 +72,8 @@ export class DemoMaterialModule { }
 
 @NgModule({
   declarations: [
-    AppComponent, ApiItemsComponent, MaterialComponent, [CrearItemCallDialog, CrearItemDialog], MostrarItems, EditarItemDialog, EditarItemCallDialog, VerItemDialog, VerItemCallDialog, [EliminarItemCallDialog, EliminarItemDialog], 
+    AppComponent, [CrearItemCallDialog, CrearItemDialog], MostrarItems, [EditarItemDialog, EditarItemCallDialog], 
+    [VerItemDialog, VerItemCallDialog], [EliminarItemCallDialog, EliminarItemDialog], 
   ],
   imports: [
     BrowserModule, HttpModule, FormsModule, DemoMaterialModule,
@@ -80,7 +82,8 @@ export class DemoMaterialModule { }
     MatNativeDateModule,
     FlexLayoutModule
   ],
-  entryComponents: [CrearItemCallDialog, CrearItemDialog , EditarItemDialog, EditarItemCallDialog, VerItemDialog,VerItemCallDialog, EliminarItemCallDialog, EliminarItemDialog],   
+  entryComponents: [CrearItemCallDialog, CrearItemDialog , EditarItemDialog, EditarItemCallDialog, 
+    VerItemDialog,VerItemCallDialog, EliminarItemCallDialog, EliminarItemDialog],   
   providers: [HttpService, ApiItemsService],
   bootstrap: [AppComponent]
 })
